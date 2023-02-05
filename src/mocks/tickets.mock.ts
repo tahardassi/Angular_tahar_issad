2efca1e68f4cd142682f5007be39c2d3cd618b64
tahar issad <issadtahar@icloud.com>
2023-02-05 20:05:36 +0100

    Exercice0-4 Modifier le model Ticket en remplaçant l’attribut author par student dans src/models/ticket.ts. mettre à jour src/app/tickets/ticket/ticket.component.html, src/app/tickets/ticket-form/ticket-form.component.ts (fonction addTicket) et src/mocks/tickets.mock.ts pour pouvoir de nouveau voir le nom de l'étudiant

--- src/mocks/tickets.mock.ts
+++ src/mocks/tickets.mock.ts
@@ -7,12 +7,12 @@ export const TICKETS_MOCKED: Ticket[] = [
     title: 'Titre 1',
     description: '',
     date: dateToday,
-    author: 'Paul'
+    student: 'Paul'
   },
   {
     title: 'Titre 2',
     description: 'Description du voyage',
     date: dateToday,
-    author: 'Anakin'
+    student: 'Anakin'
   },
 ];
