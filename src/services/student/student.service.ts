0185af08ae19d3e0ceedb28f2931dbab27cd665c
tahar issad <issadtahar@icloud.com>
2023-02-24 23:51:35 +0100

    compléter studentService

--- src/services/student/student.service.ts
+++ src/services/student/student.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import { STUDENTS_MOCKED } from 'src/mocks/student.mock';
+import { Student } from 'src/models/student';
+import { BehaviorSubject } from 'rxjs';
 @Injectable({
     providedIn: 'root'
 })
 
 export class StudentService{
+    
+    private studentList: Student[]= STUDENTS_MOCKED;
+
+
+    /*
+    * Observable which contains the list
+    */
+    public students$: BehaviorSubject<Student[]> = new BehaviorSubject(this.studentList);
     
     constructor() {
     }
