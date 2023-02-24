f9f12af03fe5a07fb7d54893a7b880cbe2d9d85d
tahar issad <issadtahar@icloud.com>
2023-02-24 23:11:00 +0100

    création d'un mock student

---
+++ src/mocks/student.mock.ts
@@ -0,0 +1,14 @@
+import { Student } from '../models/student';
+
+export const STUDENTS_MOCKED: Student[] = [
+    {
+        id : 0,
+        firstName : "Jean-Yves",
+        lastName : "Delmotte"
+    },
+    {
+        id : 1,
+        firstName:"Rémi",
+        lastName:"Pourtier"
+    }
+];
