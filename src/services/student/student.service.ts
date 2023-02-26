f1504014467eaa75cabfbae30d29a869f5ea8739
tahar issad <issadtahar@icloud.com>
2023-02-26 21:49:22 +0100

    en commentaire ce qui faut faire pour garder que la liste fournie par le serveur

--- src/services/student/student.service.ts
+++ src/services/student/student.service.ts
@@ -1,12 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Component, OnInit } from '@angular/core';
+import { BehaviorSubject,  Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 import { STUDENTS_MOCKED } from 'src/mocks/student.mock';
 import { Student } from 'src/models/student';
-import { BehaviorSubject } from 'rxjs';
+
+
+
 @Injectable({
     providedIn: 'root'
 })
 
-export class StudentService{
+export class StudentService implements OnInit{
     
     private studentList: Student[]= STUDENTS_MOCKED;
 
@@ -16,6 +21,34 @@ export class StudentService{
     */
     public students$: BehaviorSubject<Student[]> = new BehaviorSubject(this.studentList);
     
-    constructor() {
+    private studentUrl  = 'http://localhost:9428/api/students';
+
+    constructor(private http: HttpClient) {
+        (this.http.get<Student[]>(this.studentUrl)).subscribe(
+            (students: Student[]) => {
+              /*
+              * Mettre à jour la liste des étudiants/ pour utiliser que la liste fournie par le serveur 
+              * il faut juste assigner la liste retourné par getStudent() à this.studentListe
+              */
+              //this.studentList = students;//decommenter cette ligne 
+              this.studentList = this.studentList.concat( students);//et supprimer celle-ci
+
+          
+              // Mettre à jour le BehaviorSubject avec la nouvelle liste
+              this.students$.next(this.studentList);
+            },
+            (error: any) => {
+              console.error(error);
+            }
+          );
     }
+    ngOnInit() {
+    }
+   //renvoie un tableau de type Student
+    getStudent(): Observable<Student[]> {
+        return this.http.get(this.studentUrl, { responseType: 'json' }).pipe(
+            map((response: any) => response as Student[])
+        );
+    }
+      
 }
